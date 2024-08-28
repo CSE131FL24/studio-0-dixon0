c78cb1ec806321c3646aedf47c827b17b3a9a406
marinhiggins <marinhiggins@localhost>
2024-08-28 15:24:55 -0500

    jhgfds

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("hi everybody yayyyyy *<:)!");
+		System.out.println("hi everybody yayyyyyy *<:)!");
 	}
 }
