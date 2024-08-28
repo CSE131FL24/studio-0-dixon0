2019f5ade7f7b4b1f0d2eef3638b75d59de97fe4
marinhiggins <marinhiggins@localhost>
2024-08-28 15:14:06 -0500

    new message!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("hi group mates!");
+		System.out.println("hi everybody yayyyy *<:)!");
 	}
 }
